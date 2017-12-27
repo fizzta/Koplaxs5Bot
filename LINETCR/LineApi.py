74be5607ebab347e08b36f6ac583d789f9d73036
NightRaid <30490090+fizzta@users.noreply.github.com>
2017-12-27 11:00:20 +0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -44,10 +44,10 @@ class LINE:
     self.authToken = self.Talk.authToken
     self.cert = self.Talk.cert
     self._headers = {
-              'X-Line-Application': 'IOSIPAD\t7.14.0\tiPhone OS\t10.12.0', 
-              'X-Line-Access': self.authToken, 
-              'User-Agent': 'Line/7.14.0'
-   }
+          'X-Line-Application' : 'CHROMEOS 1.4.17 Chrome_OS 1', 
+          'X-Line-Access' : 'Eoaiw7nrdwmZxGSfygz7.LaPQ2EgcvCekTAknQ7bMnW.9cSvP5238SaR7iMn6jbzmOsScKhlwxt7ns5pb3VPXhw=', 
+          'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.3' 
+           }
     self.Poll = Poll(self.authToken)
     self.channel = channel.Channel(self.authToken)
     self.channel.login()	
